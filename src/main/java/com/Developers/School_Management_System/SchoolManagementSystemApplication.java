25558b3d044a273e4bd248bfffcebec90933a4a4
Ahmed Hefow <you@example.com>
2024-06-27 20:07:35 +0300

    initial commit

---
+++ src/main/java/com/Developers/School_Management_System/SchoolManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Developers.School_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Developers/School_Management_System/SchoolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Developers.School_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
