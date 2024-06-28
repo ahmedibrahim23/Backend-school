bb23a793fc329cd9328f54500c235b811d1b811e
mupashirbare <mubashirbarre12@gmail.com>
2024-06-28 03:56:33 -0700

    Teacher and Class Models

---
+++ src/main/java/com/Developers/School_Management_System/modal/Teacher.java
@@ -0,0 +1,33 @@
+package com.Developers.School_Management_System.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "TeacherTble")
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String FullName;
+
+    private Date DateOfBirth;
+
+    private String Gender;
+
+    private String Address;
+
+    private  Long  Phone;
+
+    @Column(unique = true)
+    private String Email;
+
+    private String password;
+
+    private Date HireDate;
+
+}
