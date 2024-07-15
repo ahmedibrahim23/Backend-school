9357805d66106578027e1edae3afcebe9da56fbd
ahmedibrahim23 <axmedaadan484@gmail.com>
2024-07-15 21:52:01 +0300

    pom

--- src/main/java/com/Developers/School_Management_System/Exception/ExamNotFoundException.java
+++ src/main/java/com/Developers/School_Management_System/Exception/ExamNotFoundException.java
@@ -1,4 +1,4 @@
-package com.Developers.School_Management_System.exception;
+package com.Developers.School_Management_System.Exception;
 
 public class ExamNotFoundException extends RuntimeException{
     public ExamNotFoundException(Long id){super("Class not found with id: " + id);}
